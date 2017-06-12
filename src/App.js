c756a62e2da9ecc646a5935680e3d7d56e97f809
russc <russcallahan@mac.com>
2017-06-12 11:14:08 -0500

    removes port

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import {
   createNetworkInterface,
 } from 'react-apollo';
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
-const networkInterface = createNetworkInterface({ uri: `https://dixonserver.herokuapp.com:${PORT}/graphql` });
+const networkInterface = createNetworkInterface({ uri: `https://dixonserver.herokuapp.com/graphql` });
 
 const client = new ApolloClient({
   networkInterface,
